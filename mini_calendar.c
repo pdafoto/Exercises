108f6dbc27bd950cca10563ad3c768bf4e5b323c
Nicolás Pérez de Agustín <142531556+pdafoto@users.noreply.github.com>
2025-01-29 13:39:56 +0100

    Update mini_calendar.c

--- mini_calendar.c
+++ mini_calendar.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+int days_in_month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
 bool  is_leap_year(int year)
 {
   if(year % 4 != 0)
@@ -13,13 +15,51 @@ bool  is_leap_year(int year)
     return(true);
 }
 
+void  add_days_to_date(int *mm, int *dd, int *yy, int days_left_to_add)
+{
+  int days_left_in_month;
+
+  while(days_left_to_add > 0)
+  {
+    days_left_in_month = days_in_month[*mm] - *dd;
+    if(*mm == 2 && is_leap_year(*yy) == true)
+      days_left_in_month++;
+    if(days_left_to_add > days_left_in_month)
+    {
+      days_left_to_add = days_left_to_add - (days_left_in_month + 1);
+      *dd = 1;
+      if(*mm == 12)
+      {
+        *mm = 1;
+        *yy = *yy + 1;
+      }
+      else
+        *mm = *mm + 1;
+    }
+    else
+    {
+      *dd += days_left_to_add;
+      days_left_to_add = 0;
+    }
+  }
+}
+
 int main() {
-  int year;
+  /*int year;
 
   printf("Enter a year between 1800 and 10000: ");
   scanf("%d", &year);
   if(is_leap_year(year) == true)
     printf("Leap Year\n");
   else
-    printf("Not Leap Year\n");
+    printf("Not Leap Year\n");*/
+  int mm;
+  int dd;
+  int yy;
+  int days_left_to_add;
+
+  printf("Please enter a date in the format mm dd yy and provide the number of days to add to this date: ");
+  scanf("%d%d%d%d", &mm, &dd, &yy, &days_left_to_add);
+  add_days_to_date(&mm, &dd, &yy, days_left_to_add);
+  printf("%d/%d/%d\n", mm, dd, yy);
 }
